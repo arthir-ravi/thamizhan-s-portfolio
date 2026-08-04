a70a434db073334cab8225e1e88edb5a5c1127a5
Arthi R <arthi.r@manisoftsolutions.com>
2026-08-04 15:10:32 +0530

    Updated the lawagent images and removed the unused imports

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,16 +95,13 @@ $(function () {
     }
 
     //===== close navbar-collapse when a  clicked
-    $(".navbar-nav a").on("click", function () {
-      $(".navbar-collapse").removeClass("show");
-    });
-
     $(".navbar-toggler").on("click", function () {
       $(this).toggleClass("active");
     });
 
     $(".navbar-nav a").on("click", function () {
       $(".navbar-toggler").removeClass("active");
+      $(".navbar-collapse").collapse("hide");
     });
   }
 
